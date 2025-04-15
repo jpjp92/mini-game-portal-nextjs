dbf36d647f988cdf3a4ed5a81f39dfa97c438088
Juan Park <111858577+jpjp92@users.noreply.github.com>
2025-04-15 13:05:33 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,9 +24,9 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <div className="h-screen flex flex-col items-center justify-center bg-gray-100 overflow-hidden">
       <Header />
-      <div className="w-full h-64 md:h-96 relative">
+      <div className="w-full h-64 md:h-80 relative">
         <img
           src="/home-banner.jpg"
           alt="게임 포털 배너"
