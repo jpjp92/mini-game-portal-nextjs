2c637d1c8ff59bac747c28e42780337293eb8f5b
Juan Park <111858577+jpjp92@users.noreply.github.com>
2025-04-15 13:58:23 +0900

    Update GameCard.js

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -25,9 +25,9 @@ import Link from 'next/link';
 function GameCard({ title, image, link }) {
   return (
     <div className="flex flex-col items-center justify-center m-4">
-      <Link href={link} target="_blank" rel="noopener noreferrer" className="bg-white rounded-lg shadow-lg overflow-hidden relative group block">
-        <div className="w-32 h-32 relative">
-          <Image src={image} alt={title} width={32} height={32} objectFit="cover" className="group-hover:scale-105 transition-transform duration-300" />
+      <Link href={link} target="_blank" rel="noopener noreferrer" className="w-24 h-24 bg-white rounded-lg shadow-lg overflow-hidden relative group block">
+        <div className="w-24 h-24 relative">
+          <Image src={image} alt={title} width={96} height={96} objectFit="cover" className="group-hover:scale-105 transition-transform duration-300" />
         </div>
         <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-black bg-opacity-50">
           <span className="text-white text-lg font-bold">Play Now</span>
